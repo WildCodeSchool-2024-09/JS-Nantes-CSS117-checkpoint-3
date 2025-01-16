c8852c1cc10dd7721ecac008945fc17e5df96ba8
LucasBbe <lucas.boube@gmail.com>
2025-01-16 09:55:32 +0100

    MCD, Step1 and Step2 finished

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,8 +7,13 @@ const router = express.Router();
 /* ************************************************************************* */
 
 import boatActions from "./modules/boat/boatActions";
+import tileActions from "./modules/tile/tileActions";
 
 router.get("/api/boats", boatActions.browse);
+
+router.get("/api/tiles", tileActions.browse);
+
+router.put("/api/boats/:id", boatActions.edit);
 
 import gameActions from "./modules/game/gameActions";
 
