e463441ddbc779b885d3c5e4c826fd1df9881920
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-16 11:09:34 +0100

    step 2 done

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,12 +7,17 @@ const router = express.Router();
 /* ************************************************************************* */
 
 import boatActions from "./modules/boat/boatActions";
+import tilesActions from "./modules/tile/tileActions";
 
 router.get("/api/boats", boatActions.browse);
+router.get("/api/tiles", tilesActions.browse);
 
+import tileActions from "../app/modules/tile/tileActions";
 import gameActions from "./modules/game/gameActions";
 
 router.post("/api/games", gameActions.add);
+
+router.put("/api/boats/:id", tileActions.validate, boatActions.edit);
 
 /* ************************************************************************* */
 
