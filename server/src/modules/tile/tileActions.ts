8ca040bc7a21728a27d4d42975e944f9a8cfcc54
GaeBolga <benjamin.lefort@mail.com>
2025-01-16 10:08:47 +0100

    step 2

--- server/src/modules/tile/tileActions.ts
+++ server/src/modules/tile/tileActions.ts
@@ -1,7 +1,18 @@
 import type { RequestHandler } from "express";
+import tileRepository from "./tileRepository";
 
 const browse: RequestHandler = async (req, res, next) => {
-  // your code here
+  try {
+    const answer = await tileRepository.readAll();
+
+    if (!answer) {
+      res.sendStatus(404);
+    } else {
+      res.json(answer);
+    }
+  } catch (err) {
+    next(err);
+  }
 };
 
 const validate: RequestHandler = async (req, res, next) => {
